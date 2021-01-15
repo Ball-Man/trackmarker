80ad28847dee5a4adf1dd3a6342450a5162165ab
Francesco Mistri <francesco.mistri98@gmail.com>
2021-01-15 16:22:34 +0100

    Main window

--- trackmarker/__init__.py
+++ trackmarker/__init__.py
@@ -1,4 +1,30 @@
+import pyglet
+
+window = None
+batch = None
+
+track_dict = {}
+
+
+def on_draw():
+    batch.draw()
+
+
+def on_key_press(symbol, mods):
+    """"""
+    pass
 
 
 def main():
-    pass
+    global window, batch
+    window = pyglet.window.Window()
+    batch = pyglet.graphics.Batch()
+
+    rect = pyglet.shapes.Rectangle(0, 0, window.width, window.height,
+                                   batch=batch,
+                                   color=(0xff, 0xff, 0xff))
+    rect.visible = False
+
+    window.set_handlers(on_draw, on_key_press)
+
+    pyglet.app.run()
